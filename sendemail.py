01e536ca0ac2bcca4e60bde21bd22b552f3d1e6e
Dew-Sniega <33239774+Dew-Sniega@users.noreply.github.com>
2017-11-02 15:00:47 +0800

    Update sendemail.py

--- sendemail.py
+++ sendemail.py
@@ -4,7 +4,7 @@ import email.mime.text
 
 msg = email.mime.multipart.MIMEMultipart()
 msg['from'] = 'Dew_Sniega@163.com'
-msg['to'] = 'misakiruby@126.com'
+msg['to'] = '1205620040@qq.com'
 msg['subject'] = 'test'
 content = '''''
     你好，
@@ -19,5 +19,5 @@ smtp = smtplib
 smtp = smtplib.SMTP()
 smtp.connect('smtp.163.com', '25')
 smtp.login('Dew_Sniega@163.com', 'wrwnfcw97')
-smtp.sendmail('Dew_Sniega@163.com', 'misakiruby@126.com', str(msg))
+smtp.sendmail('Dew_Sniega@163.com', '1205620040@qq.com', str(msg))
 smtp.quit()
