fea5ad6384f9b595102e980e573d48f8f5252fbc
Dew-Sniega <33239774+Dew-Sniega@users.noreply.github.com>
2017-11-09 19:32:36 +0800

    Update test1.py

--- object oriented python/test1.py
+++ object oriented python/test1.py
@@ -10,12 +10,12 @@ class Student:
       self.stu_num = stu_num
       self.name = name
       self.stu_class = stu_class
-      self.sex = male
+      self.sex = sex
       Student.stuCount += 1
    
    def displayCount(self):
      print "Total Student %d" % Student.stuCount
  
    def displayStudent(self):
-      print "Stu_num: ",self.stu_num,",Name : ", self.name,  ", Stu_class: ", self.stu_class,",Male: ",self.sex
+      print "Stu_num: ",self.stu_num,",Name : ", self.name,  ", Stu_class: ", self.stu_class,",Sex: ",self.sex
 
